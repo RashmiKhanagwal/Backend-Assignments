7f031e38435be1fa389ae751f40448e1a2324a70
RashmiKhanagwal <rashmi.khanagwal@gmail.com>
2022-02-14 14:15:49 +0530

    server file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Todo = require("./routes/todo"); 
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -12,5 +13,7 @@ mongoose
   })
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
+
+app.use("/todo",Todo);
 
 app.listen(3000, () => {console.log("server connected")})
