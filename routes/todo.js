9d930d246c38e3aaaf05a0b53cf44a4e1f3f93b8
RashmiKhanagwal <rashmi.khanagwal@gmail.com>
2022-02-14 14:10:15 +0530

    todo routes CRUD operations

---
+++ routes/todo.js
@@ -0,0 +1,77 @@
+const router = require("express").Router();
+const Todo = require("../models/todo");
+
+//CREATE TODO
+router.post("/", async (req, res) => {
+    const newTodo = new Todo(req.body);
+    try {
+      const savedTodo = await newTodo.save();
+      res.status(200).json(savedTodo);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+});
+
+// GET all todos
+router.get("/:id", async (req, res) => {
+  try {
+    const todos = await Todo.find(req.params.id);
+    res.status(200).json(todos);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET one TODO 
+router.get("/:id", async (req, res) => {
+    try {
+      const todo = await Todo.findById(req.params.id);
+      res.status(200).json(todo);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+});
+  
+//UPDATE todo
+router.put("/:id", async (req, res) => {
+    try {
+      const todo = await Todo.findById(req.params.id);
+      if (todo.username === req.body.username) {
+        try {
+          const updatedTodo = await Todo.findByIdAndUpdate(
+            req.params.id,
+            {
+              $set: req.body,
+            },
+            { new: true }
+          );
+          res.status(200).json(updatedTodo);
+        } catch (err) {
+          res.status(500).json(err);
+        }
+      } else {
+        res.status(401).json("You can update only your todo!");
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+});
+  
+//DELETE todo
+router.delete("/:id", async (req, res) => {
+    try {
+      const todo = await Todo.findById(req.params.id);
+      if (todo.username === req.body.username) {
+        try {
+          await todo.delete();
+          res.status(200).json("todo has been deleted...");
+        } catch (err) {
+          res.status(500).json(err);
+        }
+      } else {
+        res.status(401).json("You can delete only your todo!");
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+});
